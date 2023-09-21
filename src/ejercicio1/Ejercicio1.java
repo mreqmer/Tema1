3480b35448435827b4bc0c836c1c3cc517aceae8
mrequejo <mrequejo@106-22.info2>
2023-09-21 14:12:46 +0200

    Tema1

---
+++ src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,30 @@
+package ejercicio1;
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		
+		// Declaración de variables
+		Scanner sc = new Scanner(System.in);
+		short numero;
+		
+		//Pedir numero
+		 
+		System.out.println("Dime un número");
+		
+		//introducir numero
+		
+		numero = sc.nextShort();
+		
+		//Devolver numero
+		
+		System.out.println("Su número es " + numero);
+		
+		sc.close();
+	
+	
+
+	}
+
+}

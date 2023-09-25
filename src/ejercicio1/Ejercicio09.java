fc06816a27f5bb3c1c5037d8ebd0f32f8ddb29d8
mrequejo <mrequejo@106-22.info2>
2023-09-25 11:57:07 +0200

    ejercicios

--- src/ejercicio1/Ejercicio09.java
+++ src/ejercicio1/Ejercicio09.java
@@ -10,10 +10,8 @@ public class Ejercicio09 {
 		float pesetaFin;
 		
 		//Pedir datos
-		
 		System.out.println("Introduce la cantidad en €");
 		euro = sc.nextFloat();
-		
 		//Conversor
 		pesetaFin = peseta * euro;
 		System.out.println( euro +"€ en pesetas son " + pesetaFin + " pesetas.");
